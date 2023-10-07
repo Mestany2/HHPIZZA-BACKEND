6145b7e731aaa6659185de1b79ddc3425f366c94
Mestany2 <121625599+Mestany2@users.noreply.github.com>
2023-10-07 09:33:33 -0500

    Models created

---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace HHPIZZA_BACKEND.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<Order> orders { get; set; }
+    }
+}
---
+++ Models/Payment.cs
@@ -0,0 +1,8 @@
+﻿namespace HHPIZZA_BACKEND.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; } 
+        public string Type { get; set; }
+    }
+}
